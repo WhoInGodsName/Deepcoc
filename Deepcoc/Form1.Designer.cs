194b0e498f5ed9bacc3576f00b7c7492ceefda92
WhoInGodsName <61045310+WhoInGodsName@users.noreply.github.com>
2023-07-10 17:41:09 +0800

    Add project files.

---
+++ Deepcoc/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace Deepcoc
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.ControlDarkDark;
+            label1.Location = new Point(466, 18);
+            label1.Margin = new Padding(7, 0, 7, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(349, 39);
+            label1.TabIndex = 0;
+            label1.Text = "DEEPCOC MAPHATDIC 0.2";
+            label1.Click += label1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(16F, 39F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlDarkDark;
+            ClientSize = new Size(1288, 620);
+            Controls.Add(label1);
+            Font = new Font("Impact", 24F, FontStyle.Bold, GraphicsUnit.Point);
+            Margin = new Padding(7, 7, 7, 7);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+    }
+}
---
+++ Deepcoc/MemoryReader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deepcoc
+{
+    
+    internal class MemoryReader
+    {
+        [DllImport("kernel32.dll")]
+        static extern bool ReadProcessMemory(IntPtr handle, IntPtr address, [Out()] byte[] buffer, int size, out int bytesRead);
+        [DllImport("kernel32.dll")]
+        static extern bool ReadProcessMemory(IntPtr handle, ulong address, [Out()] byte[] buffer, int size, out int bytesRead);
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+    }
+}
